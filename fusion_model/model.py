b6706f056132e8c8f6f8d25997327ff3272e50b2
TheGrandNobody <thegrandnobody@protonmail.com>
2026-03-31 08:42:37 +0200

    Rank to 32

--- fusion_model/model.py
+++ fusion_model/model.py
@@ -166,7 +166,7 @@ class FusionModel(nn.Module):
     num_cross_attn_layers: int = 4
     num_attn_heads: int = 8
     num_rule_slots: int = 128
-    rule_rank: int = 16
+    rule_rank: int = 32
     history_size: int = 512
 
     def setup(self) -> None:
